4e99533e1d0c02411b3b87e4dead9ac2996b8bcf
Fight Steven <73392990+steven942693@users.noreply.github.com>
2022-08-21 12:33:35 +0000

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export default {
   title: "Fight_Steven", // 网站标题
   nickName: "真香IT", // 昵称
   githubName: "steven942693", // github账户名
-  githubRepo: 'nuxt2-blog', // 勿修改
+  githubRepo: 'steven-yee-blog', // 
   githubBranch: "master", // 勿修改
   domain: "https://blog.csdn.net/Steven_Start?spm=1000.2115.3001.5343", // rss域名
   SEO_description: "Fight_Steven的博客，真香IT",

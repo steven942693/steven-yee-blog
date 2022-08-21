7a9b1ead1e42192795bbde5f69a707fc32a5d250
Fight Steven <73392990+steven942693@users.noreply.github.com>
2022-08-21 12:18:47 +0000

    Update config.js
    更新博客基础配置

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export default {
-  title: 'yun yu yuan', // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: 'yunyuyuan', // github账户名
+  title: "Fight_Steven", // 网站标题
+  nickName: "真香IT", // 昵称
+  githubName: "steven942693", // github账户名
   githubRepo: 'nuxt2-blog', // 勿修改
-  githubBranch: 'master', // 勿修改
-  domain: 'https://blog.halberd.cn', // rss域名
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  githubBranch: "master", // 勿修改
+  domain: "https://blog.csdn.net/Steven_Start?spm=1000.2115.3001.5343", // rss域名
+  SEO_description: "Fight_Steven的博客，真香IT",
+  SEO_keywords: "Fight_Steven,Fight_Steven's blog,真香IT,真香IT的博客",
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc", // cloudflare的webanalyze
 }
